06fe4c10e5444585c692727eed94bc4ebbcddef7
Arunas Pranckevicius <APranckevicius@gmail.com>
2012-11-30 16:52:27 +0000

    Adopted for Django v1.4 and remote RabbitMQ

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 # Django settings for celeryproject project.
+import os
+import djcelery
+djcelery.setup_loader()
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -7,21 +10,30 @@ ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
 
+APPLICATION_DIR = os.path.dirname(globals()['__file__'])
+
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'dev.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': { 
+        # Add 'postgresql_psycopg2','postgresql','mysql','sqlite3','oracle'
+        'ENGINE': 'django.db.backends.sqlite3',
+        # Or path to database file if using sqlite3.
+        'NAME': 'dev.db',
+        'USER': '',       # Not used with sqlite3.
+        'PASSWORD': '',   # Not used with sqlite3.
+        'HOST': '',     # Set to empty string for localhost.
+        'PORT': '',           # Set to empty string for default.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/New_York'
+#TIME_ZONE = 'America/New_York'
+TIME_ZONE = 'GMT'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -52,15 +64,40 @@ SECRET_KEY = '^2yh8+kwlgv9q=11=*oy$tnod7-66fxjm-l6&3sdn8*wgy2^%('
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'geordi.VisorMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'linaro_django_pagination.middleware.PaginationMiddleware',
+    'common.filter_persist_middleware.FilterPersistMiddleware',
+    #'mongodb_connection_middleware.MongodbConnectionMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+)
+
+ROOT_URLCONF = 'cdr_stats.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates"
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(APPLICATION_DIR, 'templates'),
 )
 
 ROOT_URLCONF = 'urls'
@@ -72,18 +109,34 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
+    'admin_tools',
+    'admin_tools.theming',
     'django.contrib.auth',
+    'admin_tools.dashboard',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
     'people',
-    'celery',
+    'djcelery',
 )
 
 CELERY_BACKEND = "database"
-BROKER_HOST = "localhost"
-BROKER_PORT = 5672
-BROKER_USER = "myusername"
-BROKER_PASSWORD = "mypassword"
-BROKER_VHOST = "myvhost"
+
+#BROKER_HOST = "localhost"
+#BROKER_PORT = 5672
+#BROKER_USER = "myusername"
+#BROKER_PASSWORD = "mypassword"
+#BROKER_VHOST = "myvhost"
+BROKER_URL="amqp://myusername:mypassword@localhost:5672/myvhost"
+
+CELERY_DEFAULT_QUEUE = 'celery-test-project'
+CELERY_DEFAULT_EXCHANGE = "celery-test"
+CELERY_DEFAULT_EXCHANGE_TYPE = "topic"
+CELERY_DEFAULT_ROUTING_KEY = "celery"
+CELERY_QUEUES = {
+    'celery-test-project': {
+        'binding_key': '#',
+    },
+}
+
